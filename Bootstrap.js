24aac0bb1e9a93d1318ba1ce5b4eeb7082c1b869
Randazoo <dtaube02@gmail.com>
2025-02-26 15:30:42 -0500

    Add files via upload

---
+++ Bootstrap.js
@@ -0,0 +1,8 @@
+var div = document.createElement('div');
+div.textContent = 'Success';
+div.style.position = 'absolute';
+div.style.top = '10px';
+div.style.left = '10px';
+div.style.fontSize = '30px';
+div.style.color = 'green';
+document.body.appendChild(div);
